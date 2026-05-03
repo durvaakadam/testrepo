e9a090002af839382d659049abb073889bfc369a
Durva Kadam <durvakadam204@gmail.com>
2026-05-03 16:56:35 +0530

    Remove unused functions from hi.c
    Removed print_result function and main function.

--- hi.c
+++ hi.c
@@ -1,19 +1,5 @@
-#include <stdio.h>
 
 int add(int a, int b) {
     return a + b;
 }
 
-void print_result(int result) {
-    printf("Result: %d\n", result);
-}
-
-int main() {
-    int x = 5;
-    int y = 10;
-
-    int sum = add(x, y);
-    print_result(sum);
-
-    return 0;
-}
