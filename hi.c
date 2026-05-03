068562667553bbc4ed1d766bf0cf6a3e835be52a
Durva Kadam <durvakadam204@gmail.com>
2026-05-03 16:03:41 +0530

    Remove comments from add and print_result functions
    Removed comments for add and print_result functions.

--- hi.c
+++ hi.c
@@ -1,11 +1,9 @@
 #include <stdio.h>
 
-// Function to add two numbers
 int add(int a, int b) {
     return a + b;
 }
 
-// Function to print result
 void print_result(int result) {
     printf("Result: %d\n", result);
 }
