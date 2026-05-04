7d925a51109c960c7639ff8f40d87d9ae95818d1
Durva Kadam <durvakadam204@gmail.com>
2026-05-04 11:11:42 +0530

    Update hi.c

--- hi.c
+++ hi.c
@@ -1,5 +1,10 @@
+#include <limits.h>
+#include <stdio.h>
 
-int add(int a, int b) {
+int safe_add(int a, int b) {
+    if ((b > 0 && a > INT_MAX - b) || (b < 0 && a < INT_MIN - b)) {
+        printf("Overflow occurred!\n");
+        return 0;
+    }
     return a + b;
 }
-
